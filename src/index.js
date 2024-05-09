6e338a37ea795da370c7eb75f4e850b0fa15a672
Franklyn Reyes <franklynreyes777@yahoo.com>
2024-05-09 17:09:16 -0400

    Fixing based on Jonas specifications

--- src/index.js
+++ src/index.js
@@ -10,34 +10,22 @@ function App() {
     <div className="card">
       <Avatar name="./img/Branding_Profile_Image.JPG" />
       <div className="data">
-        <Intro name="As a Software Engineer, looking to create an app to automate and showcase games I've built in my spare time." />
+        <Intro
+          nameOne="As a Software Engineer, looking to create an app to automate and showcase games I've built in my spare time."
+          nameTwo="Franklyn Reyes"
+        />
         {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
-        <SkillList skillName="React-JS🏆🧑‍🚀🧠" />
-        <SkillList skillName="JavaScript💪💪💪💪" />
-        <SkillList skillName="Game Dev🏆🏆🏆" />
-        <SkillList skillName="Cypress✅🐛🐜🐞🪳" />
+        <SkillList />
+        {/* <SkillList skillName="JavaScript💪💪💪💪" {color}/> */}
+        {/* <SkillList skillName="Game Dev🏆🏆🏆" />
+        <SkillList skillName="Cypress✅🐛🐜🐞🪳" /> */}
       </div>
     </div>
   );
 }
-function SkillList({ skillName }) {
-  // const getData = dataSkills;
-  //  const numSkills = getData.length;
-  return (
-    <div
-      className="skill SkillList card"
-      style={{ backgroundColor: "white", width: 120, height: 80 }}
-    >
-      {skillName}
-    </div>
-  );
-}
 
-function Skill() {
-  return <div></div>;
-}
 function Avatar({ name }) {
   return (
     <div>
@@ -46,23 +34,55 @@ function Avatar({ name }) {
         src={name}
         // style={{ width: 445, height: 470 }}
         alt="Franklyn Reyes"
+        cy-data="worldPremier"
       />
     </div>
   );
 }
 
-function Intro({ name }) {
+function Intro({ nameOne, nameTwo }) {
   // console.log(props);
   return (
-    <div>
-      <h1>Franklyn Reyes</h1>
-      <p>{name}</p>
+    <div className="avatar">
+      <h1>{nameTwo}</h1>
+      <p>{nameOne}</p>
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+function SkillList() {
+  // const getData = dataSkills;
+  //  const numSkills = getData.length;
+  return (
+    <div
+      className="skill-list"
+      // style={{ backgroundColor: "white", width: 105, height: 26 }}
+    >
+      {/* 💪 = advnaced, 👍= intermediate 👶= beginner; */}
+      <Skill skill="React " emoji="💪" color="teal" />
+      <Skill skill="Game Dev " emoji="👍" color=" red" />
+      <Skill skill="Cypress" emoji="💪" color="lightblue" />
+      <Skill skill="Playwright" emoji="👶" color="green" />
+      <Skill skill="C Sharp" emoji="👍" color="blue" />
+      <Skill skill="Game Dev" emoji="👶" color="yellow" />
+      {/* {skillName} */}
+      {/* <Skill skill="HTML+CSS" emoji="💪" color="blue" /> */}
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      {props.skill}
+      {props.emoji}
+    </div>
+  );
+}
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
